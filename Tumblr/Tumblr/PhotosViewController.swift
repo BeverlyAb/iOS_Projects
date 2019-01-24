e7119a55a30811e74d3d994ca24d7786be946241
Beverly Abadines <BeverlyAb@users.noreply.github.com>
2019-01-23 23:25:48 -0800

    added helpers "Api, Data" and cells work

--- Tumblr/Tumblr/PhotosViewController.swift
+++ Tumblr/Tumblr/PhotosViewController.swift
@@ -30,29 +30,29 @@ class PhotosViewController: UIViewController, UITableViewDataSource, UITableView
     }
     
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Network request snippet
-        let url = URL(string: "https://api.tumblr.com/v2/blog/humansofnewyork.tumblr.com/posts/photo?api_key=Q6vHoaVm5L1u2ZAW1fqv3Jw48gFzYVg9P0vH0VHl3GVy6quoGV")!
-        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
-        session.configuration.requestCachePolicy = .reloadIgnoringLocalCacheData
-        let task = session.dataTask(with: url) { (data, response, error) in
-            if let error = error {
-                print(error.localizedDescription)
-            } else if let data = data,
-                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] {
-                
-                
-                // TODO: Get the posts and store in posts property
-                self.posts = dataDictionary["response"] as! [[String : Any]]
-                // TODO: Reload the table view
+    func getPosts(){
+        API.getPosts() { (posts) in
+            if let posts = posts{
+                self.posts = posts
                 self.tableView.reloadData()
             }
         }
-        task.resume()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
-        tableView.reloadData()
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let photoDetailVC = segue.destination as! PhotosViewController
+        
+        let cell = sender as! PhotoCell
+        
+        let image = cell.mainImage.image
+        
     }
 
 }
+
