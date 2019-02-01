5de74860513a7ea53dc9a86af7679b3bbd810cff
Beverly Abadines <BeverlyAb@users.noreply.github.com>
2019-01-31 19:33:51 -0800

    Network also works on GridViewController

--- Flix/Flix/Controllers/MovieGridViewController.swift
+++ Flix/Flix/Controllers/MovieGridViewController.swift
@@ -16,22 +16,8 @@ class MovieGridViewController: UIViewController{
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let url = URL(string: "https://api.themoviedb.org/3/movie/297762/similar?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
-        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
-        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
-        let task = session.dataTask(with: request) { (data, response, error) in
-            // This will run when the network request returns
-            if let error = error {
-                print(error.localizedDescription)
-            } else if let data = data {
-                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
-                self.myMovies = dataDictionary["results"] as! [[String:Any]]
-                self.collectionView.reloadData()
-            }
-        }
-        task.resume()
-    
+
+        getMovies()
         collectionView.delegate = self
         collectionView.dataSource = self
         
@@ -47,6 +33,16 @@ class MovieGridViewController: UIViewController{
         layout.itemSize = CGSize(width: width, height : width * 1.5)
         
     }
+    
+    func getMovies(){
+        let net = Network(urlName: "https://api.themoviedb.org/3/movie/297762/similar?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")
+        net.getNowPlaying(){ (myMovies) in
+            self.myMovies = myMovies!
+            self.collectionView.reloadData()
+        }
+    }
+    
+    
     //segue
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         let cell = sender as! UICollectionViewCell
