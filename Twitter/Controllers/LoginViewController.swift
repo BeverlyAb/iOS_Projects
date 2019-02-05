23981e0ec63828f8197f1ae9da5003a545c2c4c5
Beverly Abadines <BeverlyAb@users.noreply.github.com>
2019-02-05 11:07:44 -0800

    Forced segue to run even if not auth :(

--- Twitter/Controllers/LoginViewController.swift
+++ Twitter/Controllers/LoginViewController.swift
@@ -3,33 +3,31 @@
 //  Twitter
 //
 //  Created by Beverly Abadines on 2/3/19.
-//  Copyright © 2019 Dan. All rights reserved.
+//  Copyright © 2019 BeverlyAb. All rights reserved.
 //
 
 import UIKit
 
+
 class LoginViewController: UIViewController {
 
-    @IBAction func loginButton(_ sender: Any) {
+    @IBAction func onLoginButton(_ sender: Any) {
         let twitterUrl = "https://api.twitter.com/oauth/request_token"
-      
+        
         //sender = where is transition coming from?
         TwitterAPICaller.client?.login(url: twitterUrl, success: {
             //retain user info
             UserDefaults.standard.set(true, forKey: "userLoggedIn")
-            
-            
             self.performSegue(withIdentifier: "loginToHome", sender: self)
         }, failure: { (Error) in
             print("Could not log in")
         })
-     
-         self.performSegue(withIdentifier: "loginToHome", sender: self)
+        
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         
     }
     //appears when login page shows up
