94df3fc8cd86cd9fd05e24152595bb3980f1a541
Beverly Abadines <BeverlyAb@users.noreply.github.com>
2019-02-05 17:50:45 -0800

    Displays Alert, Has Inf Scroll =DONE

--- TwitterFromStarter/Twitter/Controllers/HomeTableViewController.swift
+++ TwitterFromStarter/Twitter/Controllers/HomeTableViewController.swift
@@ -11,16 +11,15 @@ import UIKit
 class HomeTableViewController: UITableViewController {
 
     var tweetArr = [NSDictionary]()
-    var numTweet = 10
+    var numTweet = 5
     var staticNum = 5
     let refreshController = UIRefreshControl()
+    var failedToLoad :Bool = true
     
     @IBAction func logoutButton(_ sender: Any) {
         TwitterAPICaller.client?.logout()
-        
         //dismiss =make sure Login closes back to Login, nil = don't want anything to happen once it's gone
         self.dismiss(animated: true, completion: nil)
-        
         //lose user info. Make sure to use same key
         UserDefaults.standard.set(false, forKey:"userLoggedIn")
     }
@@ -33,7 +32,7 @@ class HomeTableViewController: UITableViewController {
         refreshController.addTarget(self, action: #selector(loadTweet), for: .valueChanged)
         tableView.refreshControl = refreshController
     }
-    
+    //--------------------------------loads tweets--------------------------------
     @objc func loadTweet(){
         let homeUrl = "https://api.twitter.com/1.1/statuses/home_timeline.json"
         let myParam = ["count" : numTweet]
@@ -44,21 +43,24 @@ class HomeTableViewController: UITableViewController {
             for tweet in tweets{
                 self.tweetArr.append(tweet)
             }
-            //SUPER IMPORTANT DO NOT FORGET
-            self.tableView.reloadData()
+            self.failedToLoad = false
             print("Loaded tweets")
-        
         }, failure: { (Error) in
+            self.failedToLoad = true
             print("Could not load tweets")
+            self.createAlert(title: "Uh Oh,", message: "Tweets could not be loaded")
         })
+        
+      
+        //SUPER IMPORTANT DO NOT FORGET
+        self.tableView.reloadData()
         self.refreshController.endRefreshing()
     }
     
-    //endless scroll of tweets
+    //-------------endless scroll of tweets------------------------------------------------
     func loadInfTweet(){
         let homeUrl = "https://api.twitter.com/1.1/statuses/home_timeline.json"
         numTweet = numTweet + 5
-        
         let myParam = ["count" : numTweet]
         //tweets is the expected dict output
         TwitterAPICaller.client?.getDictionariesRequest(url: homeUrl, parameters: myParam, success: { (tweets: [NSDictionary]) in
@@ -66,39 +68,53 @@ class HomeTableViewController: UITableViewController {
             for tweet in tweets{
                 self.tweetArr.append(tweet)
             }
-            //SUPER IMPORTANT DO NOT FORGET
-            self.tableView.reloadData()
+            self.failedToLoad = false
             print("Loaded tweets")
             
         }, failure: { (Error) in
+            self.failedToLoad = true
             print("Could not load tweets")
+            self.createAlert(title: "Uh Oh,", message: "Tweets could not be loaded")
         })
+        //SUPER IMPORTANT DO NOT FORGET
+        self.tableView.reloadData()
         self.refreshController.endRefreshing()
     }
-    
+    //--------------------------calls for endless scrolling--------------------------------
     override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
-        print(indexPath.row)
-        if indexPath.row  == 4 {
-            print("Row Index ", indexPath.row)
-            staticNum = staticNum + numTweet
+        if failedToLoad{
+            print(indexPath.row)
+            if indexPath.row  + 1 == staticNum{
+                print("Row Index ", indexPath.row)
+                staticNum = staticNum + numTweet
+                 self.tableView.reloadData()
+                }
+            
+        } else{
+            if indexPath.row + 1 == tweetArr.count {
+                loadInfTweet()
+                //debug
+                self.tableView.reloadData()
+            }
         }
-//        if indexPath.row + 1 == tweetArr.count {
-//            loadInfTweet()
-//        }
     }
     
+    
+    //--------------tableview (refreshes after reloadData()--------------------------------
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "TweetCell", for: indexPath) as! TweetCell
-    
-        cell.tweetText.text = tweetArr[indexPath.row]["text"] as? String
-        let user = tweetArr[indexPath.row]["user"] as! NSDictionary
-        cell.userName.text = user["name"] as? String
-        let imageURL = URL(string: (user["profile_image_url_https"] as? String)!)
-        let data = try? Data(contentsOf: imageURL!)
+        if failedToLoad == false{
+            cell.tweetText.text = tweetArr[indexPath.row]["text"] as? String
+            let user = tweetArr[indexPath.row]["user"] as! NSDictionary
+            cell.userName.text = user["name"] as? String
+            let imageURL = URL(string: (user["profile_image_url_https"] as? String)!)
+            let data = try? Data(contentsOf: imageURL!)
 
-        if let imageData = data{
-            cell.profileImg.image = UIImage(data : imageData)
+            if let imageData = data{
+                cell.profileImg.image = UIImage(data : imageData)
+            }
         }
+        
         return cell
     }
 
@@ -107,11 +123,21 @@ class HomeTableViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        print("staticNum " ,staticNum)
-//        return staticNum
-        return tweetArr.count
+        if failedToLoad  {
+            print("staticNum " ,staticNum)
+            return staticNum
+        }
+        else{
+            return tweetArr.count
+        }
     }
-
-
-
+    //------------------------alert to show that Tweets couldn't be loaded-------------
+    func createAlert(title:String, message:String){
+        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertControllerStyle.alert)
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: { (action) in
+            alert.dismiss(animated: true, completion: nil)
+        }))
+        self.present(alert,animated: true, completion: nil)
+        tableView.reloadData()
+    }
 }
--- TwitterFromStarter/Twitter/Models/Tweet.swift
+++ TwitterFromStarter/Twitter/Models/Tweet.swift
@@ -11,18 +11,18 @@ import UIKit
 import AlamofireImage
 
 class Tweet{
-    var name : String?
-    var text: String?
-    var img : UIImageView?
+    static var oneTweet = [Tweet]()
+    var userName : String?
+    var tweetText: String?
+    var profileImg : UIImageView?
     
     init(name : String, text :String, imgURL :String){
-        self.name = name
-        self.text = text
-
-        img?.af_setImage(withURL: URL(string:imgURL)!)
+        self.userName = name
+        self.tweetText = text
+        profileImg?.af_setImage(withURL: URL(string:imgURL)!)
     }
     
     convenience init(){
-    self.init(name : "First Name Last Name", text: "No Tweets Written or Read", imgURL : "https://i.imgur.com/etjgJ2D.jpg")
+        self.init(name : "First Name Last Name", text: "No Tweets Written or Read", imgURL : "https://i.imgur.com/etjgJ2D.jpg")
     }
 }
