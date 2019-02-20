c8c2a059d033c9190b2b984ccaf669a5c68369e1
Beverly Abadines <BeverlyAb@users.noreply.github.com>
2019-02-20 14:49:16 -0800

    sign in and sign up works! also fixed main.sb

--- instagram/instagram/Controllers/LoginViewController.swift
+++ instagram/instagram/Controllers/LoginViewController.swift
@@ -2,22 +2,54 @@
 //  LoginViewController.swift
 //  instagram
 //
-//  Created by Beverly Abadines on 2/19/19.
+//  Created by Beverly Abadines on 2/20/19.
 //  Copyright © 2019 BeverlyAb. All rights reserved.
 //
 
 import UIKit
+import Parse
 
 class LoginViewController: UIViewController {
 
-    @IBAction func SignUp(_ sender: Any) {
+    @IBOutlet weak var userTextField: UITextField!
+    @IBOutlet weak var pwTextField: UITextField!
+    
+    
+    @IBAction func onSignIn(_ sender: Any) {
+        let username = userTextField.text!
+        let password = pwTextField.text!
+        
+        //user already exists, no need to create
+        //user can be user or nil
+        PFUser.logInWithUsername(inBackground: username, password: password) { (user, error) in
+            if (user != nil){
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+            } else{
+                print("Error: \(error?.localizedDescription)")
+            }
+        }
     }
     
-    @IBAction func SignIn(_ sender: Any) {
+    @IBAction func onSignUp(_ sender: Any) {
+        let user = PFUser()
+        user.username = userTextField.text
+        user.password = pwTextField.text
+        
+        //(bool, error)
+        user.signUpInBackground{ (success, error) in
+            if (success){
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+            } else {
+                print("Error: \(error?.localizedDescription)")
+            }
+        }
     }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
     }
 
 }
