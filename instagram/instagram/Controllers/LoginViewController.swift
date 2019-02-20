b7ce73702dac4c9dd158fe13f7b96ab9ae9b38fe
Beverly Abadines <BeverlyAb@users.noreply.github.com>
2019-02-20 13:55:39 -0800

    Add files via upload

---
+++ instagram/instagram/Controllers/LoginViewController.swift
@@ -0,0 +1,23 @@
+//
+//  LoginViewController.swift
+//  instagram
+//
+//  Created by Beverly Abadines on 2/19/19.
+//  Copyright © 2019 BeverlyAb. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBAction func SignUp(_ sender: Any) {
+    }
+    
+    @IBAction func SignIn(_ sender: Any) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
